8e29e2c51ac19d1c76a8761a6c9cd6b696144de7
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2022-07-10 14:51:50 -0500

    api 7

--- mdc_100_series/lib/main.dart
+++ mdc_100_series/lib/main.dart
@@ -13,7 +13,35 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:shrine/firebase_options.dart';
 
 import 'app.dart';
 
-void main() => runApp(const ShrineApp());
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  print('Got a message whilst in the foreground!');
+  print('Message data: ${message.data}');
+
+  if (message.notification != null) {
+    print('Message also contained a notification: ${message.notification}');
+  }
+});
+
+  runApp(const ShrineApp());
+}
